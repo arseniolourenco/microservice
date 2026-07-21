ff3b4da7e69f12b1885fad83f2e6740cc865fdbf
Arsenio Lourenco <arseniomauro@hotmail.com>
2026-07-21 12:32:13 +0100

    New changes on order-service

--- order-service/src/main/java/com/arseniolourenco/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/arseniolourenco/order_service/controller/OrderController.java
@@ -3,8 +3,7 @@ package com.arseniolourenco.order_service.controller;
 import com.arseniolourenco.order_service.dto.OrderRequest;
 import com.arseniolourenco.order_service.model.Order;
 import com.arseniolourenco.order_service.service.OrderService;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
+
 import io.micrometer.observation.annotation.Observed;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,17 +20,9 @@ public class OrderController {
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
-    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
-    @Retry(name = "inventory")
     @Observed()
     public String placeOrder(@Valid @RequestBody OrderRequest orderRequest) {
         Order order = orderService.placeOrder(orderRequest);
         return "Order placed successfully! Order Number: " + order.getOrderNumber();
-    }
-
-    public String fallbackMethod(OrderRequest orderRequest, Exception exception) {
-        System.err.println("Fallback triggered due to: " + exception.getMessage());
-        exception.printStackTrace();
-        return "Oops! Something went wrong, please order after some time. Reason: " + exception.getMessage();
     }
 }
