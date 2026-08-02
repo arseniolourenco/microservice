b9b9442434b386734c0656ae6bf1f078d811e031
Arsenio Lourenco <arseniomauro@hotmail.com>
2026-08-02 01:53:22 +0100

    teste

--- config-server/src/test/java/com/arseniolourenco/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/arseniolourenco/configserver/ConfigServerApplicationTests.java
@@ -3,7 +3,10 @@ package com.arseniolourenco.configserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.TestPropertySource;
+
 @SpringBootTest
+@TestPropertySource(properties = "spring.profiles.active=native")
 class ConfigServerApplicationTests {
 
     @Test
